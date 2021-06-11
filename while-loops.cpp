64f0abf739f79fbd3ea07e2d7b878299ee331531
kalehub <=>
2021-06-11 15:32:45 +0800

    while loops

---
+++ while-loops.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    // example of while-loops
+    int a = 2;
+    while(a <=6){
+        cout << a << endl;
+        a++;
+    }
+
+
+    // example of do-while-loops
+    int b = 7;
+    do {
+        cout << b << endl;
+        b++;} while( b<=6);
+
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
